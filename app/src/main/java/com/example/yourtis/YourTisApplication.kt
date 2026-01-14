e8fd26f807a775d072be582981f77e9f55df66f6
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-14 20:26:59 +0700

    implementasi YourTisApplication: inisialisasi AppContainer global untuk dependency injection manual saat startup

--- app/src/main/java/com/example/yourtis/YourTisApplication.kt
+++ app/src/main/java/com/example/yourtis/YourTisApplication.kt
@@ -1,2 +1,17 @@
 package com.example.yourtis
 
+import android.app.Application
+import com.example.yourtis.repositori.AppContainer
+import com.example.yourtis.repositori.DefaultAppContainer
+
+class YourTisApplication : Application() {
+
+    // Container untuk Dependency Injection Manual
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        // Inisialisasi container saat aplikasi pertama kali dijalankan
+        container = DefaultAppContainer()
+    }
+}
