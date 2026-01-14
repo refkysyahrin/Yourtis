e8e925c6776b17161a592a24be73797d48f619b2
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-14 20:15:57 +0700

    implementasi AppContainer: setup manual dependency injection, konfigurasi retrofit (base url), dan inisialisasi repository

--- app/src/main/java/com/example/yourtis/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/yourtis/repositori/ContainerApp.kt
@@ -1,4 +1,27 @@
 package com.example.yourtis.repositori
 
-class ContainerApp {
+import com.example.yourtis.service.YourTisApiService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface AppContainer {
+    val yourTisRepository: YourTisRepository
+}
+
+class DefaultAppContainer : AppContainer {
+
+    private val baseUrl = "http://10.0.2.2:3000/"
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(baseUrl)
+        .build()
+
+    private val retrofitService: YourTisApiService by lazy {
+        retrofit.create(YourTisApiService::class.java)
+    }
+
+    override val yourTisRepository: YourTisRepository by lazy {
+        NetworkYourTisRepository(retrofitService)
+    }
 }
