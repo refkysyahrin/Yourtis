8fb1ca0aaae2886798ec83f60b78ab7e7f8412b3
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-14 20:26:19 +0700

    buat model data User: data class serializable untuk profil pengguna, kontak, dan pemisahan role (petani/pembeli)

--- app/src/main/java/com/example/yourtis/modeldata/User.kt
+++ app/src/main/java/com/example/yourtis/modeldata/User.kt
@@ -1,4 +1,13 @@
 package com.example.yourtis.modeldata
 
-class User {
-}
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class User(
+    val id_user: Int,
+    val username: String,
+    val email: String,
+    val role: String, // "Petani" atau "Pembeli"
+    val no_hp: String,
+    val alamat: String
+)
